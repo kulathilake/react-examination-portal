4b69abeef53bc7b9b25e7a1f1a978f4e202fc795
Shehan <shehanhere@gmail.com>
2020-05-10 03:55:16 +0530

    Updates

---
+++ precache-manifest.58cd75f16ff8caf6b4d1567a4ee3881f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc04cc45bc84f99b7dc6e73ef3149da",
+    "url": "/actuatorxms/index.html"
+  },
+  {
+    "revision": "2c20613bee60e197c8de",
+    "url": "/actuatorxms/static/css/3.35fcc229.chunk.css"
+  },
+  {
+    "revision": "eb8340f13178b59b3e7d",
+    "url": "/actuatorxms/static/css/main.451dc8bf.chunk.css"
+  },
+  {
+    "revision": "f8d421e4694eb559970e",
+    "url": "/actuatorxms/static/js/0.1b1dc2b3.chunk.js"
+  },
+  {
+    "revision": "2c20613bee60e197c8de",
+    "url": "/actuatorxms/static/js/3.9d0bb3e8.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/actuatorxms/static/js/3.9d0bb3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b2dbe7be149bd46f56",
+    "url": "/actuatorxms/static/js/4.dbee1a75.chunk.js"
+  },
+  {
+    "revision": "99e0740504c5f2859ba0",
+    "url": "/actuatorxms/static/js/5.c4fbda3a.chunk.js"
+  },
+  {
+    "revision": "bc4e8311eef3ec2b7842",
+    "url": "/actuatorxms/static/js/6.81cb9705.chunk.js"
+  },
+  {
+    "revision": "7c1a2485d7a87bfc47b3",
+    "url": "/actuatorxms/static/js/7.4c840528.chunk.js"
+  },
+  {
+    "revision": "fb6e79e2001e5ddc6451",
+    "url": "/actuatorxms/static/js/8.caceacf6.chunk.js"
+  },
+  {
+    "revision": "eb8340f13178b59b3e7d",
+    "url": "/actuatorxms/static/js/main.a776e11f.chunk.js"
+  },
+  {
+    "revision": "6247b23b5b8a387b9910",
+    "url": "/actuatorxms/static/js/runtime-main.9ad87e54.js"
+  },
+  {
+    "revision": "19b0b529b6a7f8a98e316b4ee231df49",
+    "url": "/actuatorxms/static/media/container.19b0b529.svg"
+  },
+  {
+    "revision": "c753bbbc3872aaf83518f95c08af8c41",
+    "url": "/actuatorxms/static/media/standing.c753bbbc.png"
+  }
+]);
